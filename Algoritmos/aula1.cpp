4234be30e401b50dff4415f1eb06c2a779295b06
juanl186 <juanlucasarmazenamento@gmail.com>
2025-03-10 21:55:42 -0300

    Códigos desenvolvidos durante as aulas

---
+++ Algoritmos/aula1.cpp
@@ -0,0 +1,26 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+
+int	main()
+
+{
+	setlocale(LC_ALL, "Portuguese");
+	
+	
+	unsigned int ano_casamento = 0, ano_atual = 0, tempo_casado = 0;
+	
+	printf("Qual o ano do seu casamento?");
+	scanf("%u",&ano_casamento);
+	
+	printf("/n Qual o ano atual?")
+	scanf("%u", &ano_atual);
+	
+	
+	
+	
+	
+}
